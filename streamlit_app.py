c7a0f7f70ae38b61a8ee29aa9c45fbc91d88d347
Abdullah Khan Kakar <150433247+AbdullahProjects@users.noreply.github.com>
2024-03-17 21:21:33 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,8 @@
 import streamlit as st
 import pickle
 import nltk
+nltk.download('stopwords')
+nltk.download('punkt')
 from nltk.corpus import stopwords
 from nltk.stem.porter import PorterStemmer
 import string
